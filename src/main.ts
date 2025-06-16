e6cd9226d7fb277630d9ec4d55446923a3703ff4
hiroponXXX <dimension12341234@gmail.com>
2025-06-16 14:54:11 +0900

    APIが漏れていたのを修正

--- src/main.ts
+++ src/main.ts
@@ -20,14 +20,15 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth"; 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Hos1TtHt-p7gRuTSN5ARtVri826HLcI",
-  authDomain: "my-health-control-project-0.firebaseapp.com",
-  projectId: "my-health-control-project-0",
-  storageBucket: "my-health-control-project-0.firebasestorage.app",
-  messagingSenderId: "1096426987474",
-  appId: "1:1096426987474:web:d1f4d31053a8be5bacb20b",
-  measurementId: "G-N77HM26KV7",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
